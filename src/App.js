8e78cc0622f8d10be7776736c683069a3a53d461
Marcelo  Steffen <marcelo@solucap.com.br>
2020-09-18 09:07:54 -0300

    Iniciando FRONTEND

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import Header from './components/Header';
+
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Header />
+        </>
+    )
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import react from 'react';
+import { render } from 'react-dom';
+
+
+render(<h1>Hello Word</h1>, document.getElementById('app'));
+
+
+// render (<App/>, document.getElementById('App'));
